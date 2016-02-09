76ae1e78c3e681311c05db1e688c497a455e0bad
smartyining <yg1305@nyu.edu>
2016-02-09 13:44:59 -0500

    delete local normalization

--- assignment-1/result.lua
+++ assignment-1/result.lua
@@ -1,3 +1,7 @@
+----------------------------------------------------------------------
+-- This script implements load the previous saved model and
+-- generate prediction on test set
+----------------------------------------------------------------------
 require 'torch'
 require 'nn'
 require 'image'
@@ -20,27 +24,21 @@ if not paths.filep(train_file) or not paths.filep(test_file) then
    os.execute('tar xvf ' .. paths.basename(tar))
 end
 
-
-model_path='.'
-
-print("loading trained model")
+-- load previous saved model
+print '==> loading trained model'
+model_path='results'
 model = torch.load(paths.concat(model_path,'model.net'))
 
 
-data_path = 'mnist.t7'
-test_file = paths.concat(data_path, 'test_32x32.t7')
-train_file = paths.concat(data_path,'train_32x32.t7')
-
-print("loading testing data")
+print '==> loading testing data'
 loaded = torch.load(test_file, 'ascii')
-
 testData = {
    data = loaded.data,
    labels = loaded.labels,
    size = function() return tesize end
 }
 
-print("loading training data (for pre-processing)")
+print '==> loading training data (for pre-processing)'
 loaded = torch.load(train_file, 'ascii')
 trainData = {
    data = loaded.data,
@@ -53,40 +51,22 @@ f = io.open('predictions.csv', 'w')
 f:write("Id,Prediction\n")
 
 function preproc()
---preprocessing testing data 
-testData.data = testData.data:float()
-trainData.data = trainData.data:float()
+   --preprocessing testing data 
 
-print '==> preprocessing data: normalize globally'
+   testData.data = testData.data:float()
+   trainData.data = trainData.data:float()
 
-mean = trainData.data[{ {},1,{},{} }]:mean()
-std = trainData.data[{ {},1,{},{} }]:std()
+   print '==> preprocessing data: normalize globally'
 
-trainData.data[{ {},1,{},{} }]:add(-mean)
-trainData.data[{ {},1,{},{} }]:div(std)
+   mean = trainData.data[{ {},1,{},{} }]:mean()
+   std = trainData.data[{ {},1,{},{} }]:std()
 
+   trainData.data[{ {},1,{},{} }]:add(-mean)
+   trainData.data[{ {},1,{},{} }]:div(std)
 
--- Normalize test data, using the training means/stds
-testData.data[{ {},1,{},{} }]:add(-mean)
-testData.data[{ {},1,{},{} }]:div(std)
---
---
--- -- Local normalization
-print '==> preprocessing data: normalize locally'
---
--- -- Define the normalization neighborhood:
-neighborhood = image.gaussian1D(7)
---
--- -- Define our local normalization operator (It is an actual nn module, 
--- -- which could be inserted into a trainable model):
-normalization = nn.SpatialContrastiveNormalization(1, neighborhood, 1):float()
---
--- -- Normalize all channels locally:
-
-print("Normalizing test data")
-for i = 1,testData:size() do
-     testData.data[{ i,{1},{},{} }] = normalization:forward(testData.data[{ i,{1},{},{} }])
-end
+   -- Normalize test data, using the training means/stds
+   testData.data[{ {},1,{},{} }]:add(-mean)
+   testData.data[{ {},1,{},{} }]:div(std)
 
 end
 
@@ -105,23 +85,24 @@ function test()
 
    -- test over test data
    print('==> testing on test set:')
+
    for t = 1,testData:size() do
-      -- disp progress
+      -- display progress
       xlua.progress(t, testData:size())
-	f:write(t..",")
+	   f:write(t..",")
+
       -- get new sample
       local input = testData.data[t]
       input = input:double()
---      if opt.type == 'double' then input = input:double()
- --     elseif opt.type == 'cuda' then input = input:cuda() end
       local target = testData.labels[t]
 
-      -- test sample
+      -- test sample and write to file
       local pred = model:forward(input)
-val,ind=torch.max(pred,1)        
-	f:write(ind[1]%10)
-	f:write("\n")
+      val,ind=torch.max(pred,1)        
+	   f:write(ind[1]%10)
+	   f:write("\n")
    end
+   
    f.close()
 
    -- timing
@@ -129,17 +110,11 @@ val,ind=torch.max(pred,1)
    time = time / testData:size()
    print("\n==> time to test 1 sample = " .. (time*1000) .. 'ms')
 
-   -- print confusion matrix
-
-   -- update log/plot
-
    -- averaged param use?
    if average then
       -- restore parameters
       parameters:copy(cachedparams)
    end
-
-   -- next iteration:
 end
 
 preproc()
